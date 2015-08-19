ccee8f7814ba436d23833aef8fc135b197376ab1
Michael Healy <vtmhealy@gmail.com>
2015-08-19 01:10:23 -0400

    commit

--- testapp.js
+++ testapp.js
@@ -11,7 +11,7 @@ function( request, response ){
 response.writeHead( 200, {"content-type": "text/plain"} );
 response.write( "Hellow world from AWS!\n" );
 response.end();
- 
+ Crocodoc.createViewer(element, config);
  
 }
 );
@@ -21,3 +21,14 @@ server.listen( 8080 );
  
 // For logging....
 sys.puts( "Server is running on 8080" );
+
+<link rel="stylesheet" href="crocodoc.viewer.min.css" />
+<script type="text/javascript" src="jquery.min.js"></script>
+<script type="text/javascript" src="crocodoc.viewer.min.js"></script>
+<div class="viewer" style="height: 400px"></div>
+<script type="text/javascript">
+    var viewer = Crocodoc.createViewer('.viewer', {
+        url: 'https://view-api.box.com/1/sessions/3c6abc0dcf35422e8353cf9c27578d5c/assets/'
+    });
+    viewer.load();
+</script>
